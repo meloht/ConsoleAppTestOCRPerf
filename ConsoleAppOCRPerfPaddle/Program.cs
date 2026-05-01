9e4c88e2f8a6bab022aac7d1086567a611d4c9ce
luohongtao <luohongtao621@hotmail.com>
2026-05-01 12:06:48 +0800

    添加项目文件。

---
+++ ConsoleAppOCRPerfPaddle/Program.cs
@@ -0,0 +1,52 @@
+﻿using OpenCvSharp;
+using Sdcb.PaddleInference;
+using Sdcb.PaddleOCR;
+using Sdcb.PaddleOCR.Models;
+using Sdcb.PaddleOCR.Models.Local;
+
+namespace ConsoleAppOCRPerfPaddle
+{
+    internal class Program
+    {
+        const string dir = @"D:\code\model\OCRTestImages";
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            OcrTest();
+
+        }
+
+        private static void OcrTest()
+        {
+            FullOcrModel model = LocalFullModels.ChineseV5;
+
+
+            var list = Directory.GetFiles(dir);
+
+
+            using (PaddleOcrAll all = new PaddleOcrAll(model, PaddleDevice.Mkldnn())
+            {
+                AllowRotateDetection = true, /* 允许识别有角度的文字 */
+                Enable180Classification = true, /* 允许识别旋转角度大于90度的文字 */
+            })
+            {
+                System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
+                sw.Start();
+                foreach (var file in list)
+                {
+                    // Load local file by following code:
+                    using (Mat src = Cv2.ImRead(file))
+                    {
+                        PaddleOcrResult result = all.Run(src);
+                        Console.WriteLine("Detected all texts: \n" + result.Text);
+
+                    }
+                }
+                sw.Stop();
+
+                Console.WriteLine($"PaddleOCR Elapsed time:{sw.Elapsed}");
+
+            }
+        }
+    }
+}
---
+++ ConsoleAppTestOCRPerf/Program.cs
@@ -0,0 +1,43 @@
+﻿using RapidOCRSharpOnnx;
+using RapidOCRSharpOnnx.Configurations;
+using RapidOCRSharpOnnx.Providers;
+using RapidOCRSharpOnnx.Utils;
+using System.Diagnostics;
+
+namespace ConsoleAppTestOCRPerf
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            TestParallelBatch();
+        }
+
+
+        private static void TestParallelBatch()
+        {
+
+            //string detectPath = @"D:\code\RapidOCR-3.8.0\python\rapidocr\models\ch_PP-OCRv4_det_mobile.onnx";
+            //string recogPath = @"D:\code\RapidOCR-3.8.0\python\rapidocr\models\ch_PP-OCRv4_rec_mobile.onnx";
+            //string clsPath = @"D:\code\RapidOCR-3.8.0\python\rapidocr\models\ch_ppocr_mobile_v2.0_cls_mobile.onnx";
+
+            //string saveDir = null;
+            string detectPath = @"D:\code\RapidOCRSharpOnnx\RapidOCRSharpOnnx.TestCommon\Models\ch_PP-OCRv5_det_mobile.onnx";
+            string recogPath = @"D:\code\RapidOCRSharpOnnx\RapidOCRSharpOnnx.TestCommon\Models\ch_PP-OCRv5_rec_mobile.onnx";
+            string clsPath = @"D:\code\RapidOCRSharpOnnx\RapidOCRSharpOnnx.TestCommon\Models\ch_PP-LCNet_x0_25_textline_ori_cls_mobile.onnx";
+          
+
+            using RapidOCRSharp ocr = new RapidOCRSharp(new ExecutionProviderCPU(new OcrConfig(detectPath, recogPath, LangRec.CH, OCRVersion.PPOCRV5, clsPath)));
+            var list = Directory.GetFiles(@"D:\code\model\OCRTestImages");
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            var resPath = ocr.BatchParallelAsync(list.ToList());
+            sw.Stop();
+            Console.WriteLine($"RapidOCRSharp Elapsed Time: {sw.ElapsedMilliseconds} ms");
+
+
+            Console.WriteLine("end");
+        }
+    }
+}
